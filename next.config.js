9c27e7959cd5702d7c7a90c38e8f22370cd4b928
Biosolver <asperant001@gmail.com>
2026-04-06 11:07:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        '@react-native-async-storage/async-storage': false,
-        'pino-pretty': false,
-        fs: false,
-        net: false,
-        tls: false,
-        encoding: false,
-      }
-    }
-    return config
-  },
-  // Отключаем строгую типизацию при сборке (временное решение)
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Отключаем линтинг при сборке
   eslint: {
     ignoreDuringBuilds: true,
   },
