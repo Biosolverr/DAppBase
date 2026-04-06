e40aec88f04925b582f55a86368a6d3fafd001de
Biosolver <asperant001@gmail.com>
2026-04-07 00:54:08 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { InitiateSwap } from '@/components/InitiateSwap'
+import { WithdrawPanel } from '@/components/WithdrawPanel'
 import { useState, useEffect } from 'react'
 import { useAccount, useConnect, useDisconnect, useReadContract } from 'wagmi'
 import { baseAccount } from '@wagmi/connectors'
